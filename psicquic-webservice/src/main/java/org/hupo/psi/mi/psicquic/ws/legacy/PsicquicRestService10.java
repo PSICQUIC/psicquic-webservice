72ea92f6d04d6cd4fc00d3e4279da1d0cb3d6f10
brunoaranda <brunoaranda@gmail.com>
2010-01-25 13:32:21 +0000

    Keeping all REST service for backwards compatibility

---
+++ psicquic-webservice/src/main/java/org/hupo/psi/mi/psicquic/ws/legacy/PsicquicRestService10.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2008 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hupo.psi.mi.psicquic.ws.legacy;
+
+import org.hupo.psi.mi.psicquic.NotSupportedMethodException;
+import org.hupo.psi.mi.psicquic.NotSupportedTypeException;
+import org.hupo.psi.mi.psicquic.PsicquicServiceException;
+
+import javax.ws.rs.*;
+
+/**
+ * RESTful web service.
+ *
+ * v.1.0/search/query/brca2
+ * current/search/query/species:human?format=xml25&firstResult=50&maxResults=100
+ *
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id: IntactPsicquicService.java 12873 2009-03-18 02:51:31Z baranda $
+ */
+@Path("/search")
+public interface PsicquicRestService10 {
+
+    @GET
+    @Path("/interactor/{interactorAc}")
+    Object getByInteractor(@PathParam("interactorAc") String interactorAc,
+                           @DefaultValue("") @QueryParam("db") String db,
+                           @DefaultValue("tab25") @QueryParam("format") String format,
+                           @DefaultValue("0") @QueryParam("firstResult") String firstResult,
+                           @DefaultValue("2147483647") @QueryParam("maxResults") String maxResults) throws PsicquicServiceException,
+            NotSupportedMethodException,
+            NotSupportedTypeException;
+
+    @GET
+    @Path("/interaction/{interactionAc}")
+    Object getByInteraction(@PathParam("interactionAc") String interactionAc,
+                            @DefaultValue("") @QueryParam("db") String db,
+                            @DefaultValue("tab25") @QueryParam("format") String format,
+                            @DefaultValue("0") @QueryParam("firstResult") String firstResult,
+                            @DefaultValue("2147483647") @QueryParam("maxResults") String maxResults) throws PsicquicServiceException,
+            NotSupportedMethodException,
+            NotSupportedTypeException;
+
+    @GET
+    @Path("/query/{query}")
+    Object getByQuery(@PathParam("query") String query,
+                      @DefaultValue("tab25") @QueryParam("format") String format,
+                      @DefaultValue("0") @QueryParam("firstResult") String firstResult,
+                      @DefaultValue("2147483647") @QueryParam("maxResults") String maxResults) throws PsicquicServiceException,
+            NotSupportedMethodException,
+            NotSupportedTypeException;
+
+    @GET
+    @Path("/formats")
+    Object getSupportedFormats() throws PsicquicServiceException,
+            NotSupportedMethodException,
+            NotSupportedTypeException;
+
+    @GET
+    @Path("/version")
+    String getVersion();
+
+}
