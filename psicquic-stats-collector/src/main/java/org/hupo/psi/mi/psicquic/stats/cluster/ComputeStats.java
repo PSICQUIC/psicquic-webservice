dec3ebf45ff060c04b3c0c006a203f52350ac046
samuel.kerrien <samuel.kerrien@gmail.com>
2010-07-02 12:56:56 +0000

    Stats are now written into a CSV file for easier processing.

--- psicquic-stats-collector/src/main/java/org/hupo/psi/mi/psicquic/stats/cluster/ComputeStats.java
+++ psicquic-stats-collector/src/main/java/org/hupo/psi/mi/psicquic/stats/cluster/ComputeStats.java
@@ -1,25 +1,38 @@
 package org.hupo.psi.mi.psicquic.stats.cluster;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import uk.ac.ebi.enfin.mi.cluster.ClusterContext;
 import uk.ac.ebi.enfin.mi.cluster.EncoreInteraction;
 import uk.ac.ebi.enfin.mi.cluster.cache.CacheManager;
 import uk.ac.ebi.enfin.mi.cluster.cache.CacheStrategy;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * TODO document this !
+ * Browse a cache of clustered interactions and for each PSICQUIC service represented calculates 3 things:
+ * <p/>
+ * 1. how many binary interactions are only seen in that service
+ * <p/>
+ * 2. how many binary interaction are seen multiple times in that service
+ * <p/>
+ * 3. how many binary interactions are seen in this service and at least one other
  *
  * @author Samuel Kerrien (skerrien@ebi.ac.uk)
  * @version $Id$
- * @since TODO add POM version
+ * @since 0.1
  */
 public class ComputeStats {
+    
+    private static final Log log = LogFactory.getLog( ComputeStats.class );
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
 
         final ClusterContext context = ClusterContext.getInstance();
         context.setCacheStrategy( CacheStrategy.ON_DISK );
@@ -28,10 +41,10 @@ public class ComputeStats {
 
         final File cacheLocation = new File( "all_psicquic.cache" );
         ClusterContext.getInstance().getCacheManager().setCacheStorage( cacheLocation );
-        System.out.println( "Processing cache stored in: " + cacheLocation.getAbsolutePath() );
+        log.info( "Processing cache stored in: " + cacheLocation.getAbsolutePath() );
 
         final Map<Integer, EncoreInteraction> cache = cacheManager.getInteractionCache();
-        System.out.println( "cluster count: " + cache.size() );
+        log.info( "cluster count: " + cache.size() );
 
         final Map<String, ServiceStat> service2stats = new HashMap<String, ServiceStat>();
 
@@ -41,7 +54,7 @@ public class ComputeStats {
 
             EncoreInteraction interaction = cache.get( i );
 
-            System.out.println( i + "" + clusterCount );
+            log.info( i + "" + clusterCount );
 
             final Map<String, Integer> service2count = new HashMap<String, Integer>();
 
@@ -49,7 +62,7 @@ public class ComputeStats {
                 // TODO why is that a list ???
                 final String interactionAc = dbEntry.getKey();
                 final List<String> serviceName = dbEntry.getValue();
-//                System.out.println( "\t" + interactionAc + " -> " + serviceName );
+//                log.info( "\t" + interactionAc + " -> " + serviceName );
                 for ( String name : serviceName ) {
 
                     if ( !service2count.containsKey( name ) ) {
@@ -58,7 +71,7 @@ public class ComputeStats {
                     int count = service2count.get( name );
                     Integer newCount = count + 1;
                     service2count.put( name, newCount );
-//                    System.out.println( "\t\t" + name + ":" + newCount );
+//                    log.info( "\t\t" + name + ":" + newCount );
                 }
             } // evidences
 
@@ -99,19 +112,45 @@ public class ComputeStats {
 
         } // clusters
 
-        System.out.println( "" );
-        System.out.println( "--- Final Stats ---" );
+        log.info( "" );
+        log.info( "--- Final Stats ---" );
         printStats( service2stats );
 
-        System.out.println( "Collected stats about " + service2stats.size() + " services" );
+        export2csv( service2stats );
+
+        log.info( "Collected stats about " + service2stats.size() + " services" );
 
         ClusterContext.getInstance().getCacheManager().shutdown();
+    }
+
+    public static final String NEW_LINE = "\n";
+    
+    private static void export2csv( Map<String, ServiceStat> service2stats ) throws IOException {
+        StringBuilder header = new StringBuilder(256);
+        StringBuilder uniq = new StringBuilder(256);
+        StringBuilder within = new StringBuilder(256);
+        StringBuilder others = new StringBuilder(256);
+
+        for ( Map.Entry<String, ServiceStat> serviceStatEntry : service2stats.entrySet() ) {
+            final ServiceStat stat = serviceStatEntry.getValue();
+            header.append( stat.getName() ).append(',');
+            uniq.append( stat.getUniqueCount() ).append(',');
+            within.append( stat.getName() ).append(',');
+            others.append( stat.getName() ).append(',');
+        }
+
+        BufferedWriter out = new BufferedWriter( new FileWriter( new File( "stats.csv" )) );
+        out.write( header.append( NEW_LINE ).toString() );
+        out.write( uniq.append( NEW_LINE ).toString() );
+        out.write( within.append( NEW_LINE ).toString() );
+        out.write( others.append( NEW_LINE ).toString() );
+        out.close();
     }
 
     private static void printStats( Map<String, ServiceStat> service2stats ) {
         for ( Map.Entry<String, ServiceStat> serviceStatEntry : service2stats.entrySet() ) {
             final ServiceStat stat = serviceStatEntry.getValue();
-            System.out.println( stat );
+            log.info( stat );
         }
     }
 }
