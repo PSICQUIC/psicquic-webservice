34a6ab3539df993be4a7f8118cc71ea20ff46631
brunoaranda <brunoaranda@gmail.com>
2011-01-24 12:45:17 +0000

    Fixed perl script

--- psicquic-webservice/src/example/perl/read-all-psicquic.pl
+++ psicquic-webservice/src/example/perl/read-all-psicquic.pl
@@ -2,8 +2,6 @@
 use strict;
 use LWP::Simple;
 
-# ------------------ MITAB FUNCTIONS ------------------
-
 sub getXrefByDbName {
     my ( $xrefs, $dbName ) = @_;
     for my $xref (split (/\|/, $xrefs)){
@@ -11,27 +9,64 @@ sub getXrefByDbName {
          if( $db eq $dbName ) {
             return $id;
          }
-    }
-
+    }    
     return $xrefs;
 }
 
-# -----------------------------------------------------
-# Note that we are only going to get 10 interactions at most
-my $queryUrl = "http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/query/BBC1?firstResult=0&maxResults=10";
+sub readPsicquic {
+    my ( $url, $query, $from, $size ) = @_;
+    
+    # Remove the trailing /psicquic at the end of the URL so we can build  a REST URL.
+    my $finalUrl = $url =~ s/\/psicquic$//;
+    my $queryUrl = $url . "/current/search/query/" . $query . "?firstResult=" . $from . "&maxResults=" . $size;
+    
+    my $content = get $queryUrl;
+    die "Couldn't get $queryUrl" unless defined $content;
 
-my $content = get $queryUrl;
-die "Couldn't get $queryUrl" unless defined $content;
-
-#print $content;
-
-# Now list all interactions
-my @lines = split(/\n/, $content);
-for my $line (@lines) {
-  my @flds = split(/\t/, $line); # split tab delimited lines
-
-  # split fields of a PSIMITAB 2.5 line
-  my ($idA, $idB, $altIdA, $altIdB, $aliasA, $aliasB, $detMethod, $author, $pub, $orgA, $orgB, $intType, $sourceDb, $intId, $conf) = @flds;
-
-  print getXrefByDbName($idA, "uniprotkb") . " interacts with " . getXrefByDbName($idB, "uniprotkb") . "\n";
+    # Now list all interactions
+    my @lines = split(/\n/, $content);
+    my $count = @lines;
+    for my $line (@lines) {
+      my @flds = split(/\t/, $line); # split tab delimited lines
+      
+      # split fields of a PSIMITAB 2.5 line
+      my ($idA, $idB, $altIdA, $altIdB, $aliasA, $aliasB, $detMethod, $author, $pub, $orgA, $orgB, $intType, $sourceDb, $intId, $conf) = @flds;
+      
+      print getXrefByDbName($idA, "uniprotkb") . " interacts with " . getXrefByDbName($idB, "uniprotkb") . "\n";
+    }
+    
+    return $count;
 }
+
+################################################################################
+# Registry URL that lists all ACTIVE PSICQUIC services
+# Documentation: http://code.google.com/p/psicquic/wiki/Registry
+my $registryUrl = 'http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=ACTIVE&format=txt';
+
+my ($miql) = @ARGV;
+print "Searching for '$miql'...\n\n";
+
+my $registryContent = get $registryUrl;
+die "Couldn't get $registryUrl" unless defined $registryContent;
+
+# Now list all services
+my $totalCount = 0;
+my @services = split(/\n/, $registryContent);
+for my $service (@services){
+  my @flds = split(/=/, $service);
+  my ($serviceName, $serviceUrl) = @flds;
+  print "$serviceName  --->  $serviceUrl\n";
+  
+    my $current = 0;
+    my $lineCount;
+    do {
+          # Download a MITAB page (200 lines max per read)          
+          $lineCount = readPsicquic $serviceUrl, $miql, $current, 200;
+          $current = $current + $lineCount;
+    } while( $lineCount == 200 );
+    
+    print "Total: " . $current . " interaction(s).\n\n";
+    $totalCount = $totalCount + $current;
+}
+
+print "Retreived " . $totalCount . " binary interaction(s) across " . @services . " service(s)\n\n";
