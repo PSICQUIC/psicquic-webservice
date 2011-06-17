cc09de91160cce6c4229d8df74fefadec0f3ea13
brunoaranda <brunoaranda@gmail.com>
2011-06-17 13:35:37 +0000

    Issue 22: 	Export to BioPAX

--- psicquic-webservice/src/main/java/org/hupo/psi/mi/psicquic/ws/IndexBasedPsicquicService.java
+++ psicquic-webservice/src/main/java/org/hupo/psi/mi/psicquic/ws/IndexBasedPsicquicService.java
@@ -46,7 +46,7 @@ import java.util.*;
  * @version $Id$
  */
 @Controller
-@Features(features = { "org.apache.cxf.transport.http.gzip.GZIPFeature" })
+@Features(features = { "org.apache.cxf.transport.common.gzip.GZIPFeature" })
 public class IndexBasedPsicquicService implements PsicquicService {
 
     private final Logger logger = LoggerFactory.getLogger(IndexBasedPsicquicService.class);
@@ -54,6 +54,7 @@ public class IndexBasedPsicquicService implements PsicquicService {
     public static final String RETURN_TYPE_XML25 = "psi-mi/xml25";
     public static final String RETURN_TYPE_MITAB25 = "psi-mi/tab25";
     public static final String RETURN_TYPE_MITAB25_BIN = "psi-mi/tab25-bin";
+    public static final String RETURN_TYPE_BIOPAX = "biopax";
     public static final String RETURN_TYPE_COUNT = "count";
 
     private static final String NEW_LINE = System.getProperty("line.separator");
@@ -61,7 +62,7 @@ public class IndexBasedPsicquicService implements PsicquicService {
     private static final int BLOCKSIZE_MAX = 200;
     private static final String RETURN_TYPE_DEFAULT = RETURN_TYPE_MITAB25;
 
-    public static final List<String> SUPPORTED_RETURN_TYPES = Arrays.asList(RETURN_TYPE_XML25, RETURN_TYPE_MITAB25, RETURN_TYPE_COUNT);
+    public static final List<String> SUPPORTED_RETURN_TYPES = Arrays.asList(RETURN_TYPE_XML25, RETURN_TYPE_MITAB25, RETURN_TYPE_BIOPAX, RETURN_TYPE_COUNT);
 
     @Autowired
     private PsicquicConfig config;
