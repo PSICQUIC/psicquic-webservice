ed09db07369b6602461a0551fee169f7511d54dd
brunoaranda <brunoaranda@gmail.com>
2011-11-04 14:03:56 +0000

    [maven-scm] copy for tag psicquic-webservice-1.2.0

--- psicquic-registry/src/main/java/org/hupo/psi/mi/psicquic/ols/soap/QueryService.java
+++
@@ -1,9 +0,0 @@
-package org.hupo.psi.mi.psicquic.ols.soap;
-
-public interface QueryService extends javax.xml.rpc.Service {
-    public java.lang.String getOntologyQueryAddress();
-
-    public org.hupo.psi.mi.psicquic.ols.soap.Query getOntologyQuery() throws javax.xml.rpc.ServiceException;
-
-    public org.hupo.psi.mi.psicquic.ols.soap.Query getOntologyQuery(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
-}
