b360ce164d6bbb37fc6d50fdbcaa72ef5e383130
mdumousseau@yahoo.com <marine3@users.noreply.github.com>
2012-09-17 14:39:34 +0000

    startPsicquicServerOnly : fixed one property only

--- psicquic-solr-ws/startPsicquicServerOnly.sh
+++ psicquic-solr-ws/startPsicquicServerOnly.sh
@@ -4,7 +4,7 @@ if [ $# == 1 ];
 then
       SOLR_URL=$1;
       echo "SOLR server url: ${SOLR_URL}"
-      mvn clean jetty:run -Dmaven.test.skip -Dsolr.url=${SOLR_URL}
+      mvn clean jetty:run -Dmaven.test.skip -DsolrUrl=${SOLR_URL}
 else
       echo "SOLR server url not provided, use default http://localhost:9090/solr"
       mvn clean jetty:run -Dmaven.test.skip
