8cd4599f1f474cb5403a7d684223ee05cbbeb256
mdumousseau@yahoo.com <marine3@users.noreply.github.com>
2012-12-05 16:20:45 +0000

    Added ShutdownHook for releasing orchestra datasources connection manager

---
+++ psicquic-view/src/main/java/org/hupo/psi/mi/psicquic/view/webapp/application/ShutDownHook.java
@@ -0,0 +1,27 @@
+package org.hupo.psi.mi.psicquic.view.webapp.application;
+
+import org.apache.myfaces.orchestra.connectionManager.ConnectionManagerDataSource;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * TODO comment this
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>05/12/12</pre>
+ */
+
+public class ShutDownHook implements ServletContextListener{
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+        ConnectionManagerDataSource.releaseAllBorrowedConnections();
+    }
+}
